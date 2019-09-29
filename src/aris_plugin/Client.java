2f7cc32ce27e74438488c0b30a1bef6df907da05
d.efimov <d.efimov@fun-box.ru>
2019-09-29 11:17:34 +0300

    Отправть инструкции

--- src/aris_plugin/Client.java
+++ src/aris_plugin/Client.java
@@ -1,14 +1,15 @@
 package aris_plugin;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.omg.CORBA.portable.OutputStream;
-
 public class Client {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
         System.out.println("ARIS plugin");
         URL url = new URL ("http://indieteam.online/api/v1/instructions");
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
@@ -16,7 +17,20 @@ public class Client {
         con.setRequestProperty("Content-Type", "application/json; utf-8");
         con.setRequestProperty("Accept", "application/json");
         con.setDoOutput(true);
-        String jsonInputString = "{}";
+        String jsonInputString = "{      \"name\": \"Инструкция по сбору X баннера\",\n" + 
+        		"      \"details\": [\n" + 
+        		"        {\n" + 
+        		"          \"count\": 2,\n" + 
+        		"          \"name\": \"Пластиковая ножка с держателем ролла\",\n" + 
+        		"          \"shortName\": \"A\",\n" + 
+        		"          \"link\": \"http://teamcenter.indieteam.ru/1.zip\"\n" + 
+        		"        }\n" + 
+        		"      ],\n" + 
+        		"      \"steps\": [\n" + 
+        		"        {\n" + 
+        		"          \"description\": \"Взять деталь A\"\n" + 
+        		"        }\n" + 
+        		"      ]}";
         try(OutputStream os = (OutputStream) con.getOutputStream()) {
             byte[] input = jsonInputString.getBytes("utf-8");
             os.write(input, 0, input.length);           
@@ -25,8 +39,11 @@ public class Client {
         		  new InputStreamReader(con.getInputStream(), "utf-8"))) {
         		    StringBuilder response = new StringBuilder();
         		    String responseLine = null;
-        		    
-        		    
-        		}))
+        		    while ((responseLine = br.readLine()) != null) {
+        		        response.append(responseLine.trim());
+        		    }
+        		    System.out.println(response.toString());
+        		}
+        
     }
 }
